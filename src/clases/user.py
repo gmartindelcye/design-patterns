5ef2e32ed59059ccec3f813b82020a86362a0289
Gabriel Martin del Campo y Eternod <gmartindelcye@gmail.com>
2022-10-04 12:18:10 -0500

    firrt class user

---
+++ src/clases/user.py
@@ -0,0 +1,5 @@
+class User:
+    # properties
+    name = ''
+
+    # methods
---
+++ src/main.py
@@ -0,0 +1,10 @@
+from clases.user import User
+
+def main():
+    user = User()
+    user.name = "John"
+    print(user.name)
+
+
+if __name__ == '__main__':
+    main()

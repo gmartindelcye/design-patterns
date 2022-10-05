f711deb3d8988b8b823f192077c28bf5fcc183b1
Gabriel Martin del Campo y Eternod <gmartindelcye@gmail.com>
2022-10-05 16:24:43 -0500

    sayhello method

--- src/clases/user.py
+++ src/clases/user.py
@@ -2,3 +2,7 @@ class User:
     # methods
     def __init__(self, name: str = '') -> None:
         self.name = name
+
+
+    def sayHello(self) -> None:
+        print(f"Hi, my name is {self.name}")
--- src/main.py
+++ src/main.py
@@ -1,10 +1,9 @@
 from clases.user import User
 
 def main():
-    user = User('John')
-    print(user.name)
-    user0 = User()
-    print(user0.name)
+    user = User('Mary')
+    user.sayHello()
+
 
 
 if __name__ == '__main__':
